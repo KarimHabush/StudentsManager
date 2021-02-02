0c191b4bece239ec544f66052f374b272ad47a29
whiteh34rt <54319779+whiteh34rt@users.noreply.github.com>
2021-02-03 00:05:05 +0100

    Update db_connection.py

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -15,14 +15,17 @@ def get_students():
   result = db_cursor.fetchall()
   return result
 
-def add_student(name,mark): 
+def add_student(name,mark):
+  #Potential SQLi [security test]
+  #Use prepared statement
   query = "INSERT INTO employees (name, mark) VALUES (%s, %s)"
   data = (name,mark)
   db_cursor.execute(query, data)
   result = db_cursor.fetchall()
   return result
-
-
-
+def delete_student(id):
+  query = "DELETE FROM students where id = %int"
+  result = db_cursor.execute(query)
+  return result
 
 sys.modules["db_cursor"] = db_cursor
