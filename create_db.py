0e699e1072eecfc7593bc1128c49db76f42cf74f
karimhabush <karim.habush@gmail.com>
2021-02-02 21:13:44 +0100

    Create database setup script

---
+++ create_db.py
@@ -0,0 +1,22 @@
+import mysql.connector
+
+#establish the connection
+conn = mysql.connector.connect(user='root', password='root', host='localhost')
+
+#Create a cursor object using the cursor() method
+cursor = conn.cursor()
+
+#Create Database
+cursor.execute("DROP database IF EXISTS students_manager;")
+cursor.execute("CREATE database students_manager;")
+cursor.execute("use students_manager;")
+
+#Create Tables 
+cursor.execute("CREATE TABLE students(id int unsigned not null auto_increment, firstname varchar(20) not null, mark double not null, primary key (id));")
+
+cursor.execute("describe students;")
+print(cursor.fetchall())
+
+
+#Closing the connection
+conn.close()
