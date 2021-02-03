61b0727ef53e658205d5879a35803e3c5e455da7
karimhabush <karim.habush@gmail.com>
2021-02-03 17:22:32 +0100

    Add validation to Frontend

--- frontend/app.py
+++ frontend/app.py
@@ -1,13 +1,18 @@
-from flask import Flask, render_template, request,redirect
+from flask import Flask, render_template, request,redirect, url_for
 import requests
 import json
 
 app = Flask(__name__, template_folder='.')
 @app.route('/')
 def homepage():
+    msg=""
     r = requests.get(
         'http://localhost:5000/students')
-    return render_template('./templates/index.html',students=r.json()['result'])
+    if request.args.get("status")=="failure":
+        msg="L'opération a échoué, vérifiez vos données et réessayez!"
+    elif request.args.get("status")=="success":
+        msg="L'opération a bien été executé!"
+    return render_template('./templates/index.html',students=r.json()['result'],messages=msg)
 
 @app.route('/delete',methods=['POST'])
 def delete():
@@ -16,8 +21,8 @@ def delete():
     } 
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
-    requests.post('http://localhost:5000/students/delete', json=data, headers=headers)
-    return redirect("/")
+    res = requests.post('http://localhost:5000/students/delete', json=data, headers=headers)
+    return redirect(url_for(".homepage",status=json.loads(res.text)['status']))
 
 @app.route('/update',methods=['POST'])
 def update():
@@ -28,8 +33,8 @@ def update():
     } 
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
-    requests.post('http://localhost:5000/students/update', json=data, headers=headers)
-    return redirect("/")
+    res = requests.post('http://localhost:5000/students/update', json=data, headers=headers)
+    return redirect(url_for(".homepage",status=json.loads(res.text)['status']))
 
 @app.route('/add',methods=['POST'])
 def add():
@@ -39,8 +44,9 @@ def add():
     } 
     headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
 
-    requests.post('http://localhost:5000/students/add', json=data, headers=headers)
-    return redirect("/")
+    res = requests.post('http://localhost:5000/students/add', json=data, headers=headers)
+    print(res.text)
+    return redirect(url_for(".homepage",status=json.loads(res.text)['status']))
 
 
 if __name__ == '__main__':
