de3c6bc7313fb0f0d81fe9add909bcb132929043
kali <karim.habush@gmail.com>
2021-02-15 15:50:09 -0500

    Add db script

---
+++ db/init.sql
@@ -0,0 +1,6 @@
+DROP database IF EXISTS students_manager;
+CREATE DATABASE students_manager;
+use students_manager;
+
+CREATE TABLE students(id int unsigned not null auto_increment, name varchar(100) not null, mark double not null, primary key (id));
+
